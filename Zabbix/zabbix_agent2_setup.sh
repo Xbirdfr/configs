38e327fdd4c3b3f35f549f9cbcbbc1f228681bb5
Xbird <xbird.xbird@gmail.com>
2020-05-28 21:11:14 +0200

    Create zabbix_agent2_setup.sh

---
+++ Zabbix/zabbix_agent2_setup.sh
@@ -0,0 +1,7 @@
+apt purge -y zabbix-agent
+tmpdir = 'zabbixsetuptmp'
+cd $tmpdir
+sudo wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+$(lsb_release -sc)_all.deb
+sudo dpkg -i zabbix-release_5.0-1+$(lsb_release -sc)_all.deb
+apt update
+apt install -y zabbix-agent2
