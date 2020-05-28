2e0b8b8855f3315a88d4242e9fba4ae46e5c74d6
Xbird <xbird.xbird@gmail.com>
2020-05-28 20:40:29 +0200

    Create zabbix_psk_setup.sh

---
+++ Zabbix/zabbix_psk_setup.sh
@@ -0,0 +1,21 @@
+echo 'Xbird.me Zabbix PSK setup'
+echo '----------------------------------------'
+echo 'Answer custom parameters, or enter to usse default config'
+read -e -i "/etc/zabbix" -p "Config Path: " path
+read -e -i "zabbix_agent2.conf.test" -p "Zabbix agent config file: " config_file_name
+read -e -i "zabbix_agentd.psk.test" -p "Zabbix agent PSK file: " psk_file_name
+read -e -i $HOSTNAME -p "PSK identity  (you have to set the same in Zabbix host configuration) : " pskidentity
+
+
+cd $path
+openssl rand -hex 32 > $psk_file_name
+echo 'TLSConnect=psk' >> $config_file_name
+echo 'TLSAccept=psk' >> $config_file_name
+echo "TLSPSKFile=$path/$psk_file_name" >> $config_file_name
+echo "TLSPSKIdentity=$pskidentity" >> $config_file_name
+
+echo '----------------------------------------'
+echo 'Please config your Zabbix host with this :'
+echo "PSK Identity : $pskidentity"
+echo "PSK Key : $(cat $psk_file_name)"
+echo '----------------------------------------'
