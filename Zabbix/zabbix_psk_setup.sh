e51289769731ad86972fb93fffda9787a7bf9c56
Xbird <xbird.xbird@gmail.com>
2020-05-28 20:46:21 +0200

    Update zabbix_psk_setup.sh

--- Zabbix/zabbix_psk_setup.sh
+++ Zabbix/zabbix_psk_setup.sh
@@ -2,6 +2,13 @@ echo 'Xbird.me Zabbix PSK setup'
 echo '----------------------------------------'
 echo 'Answer custom parameters, or enter to usse default config'
 read -e -i "/etc/zabbix" -p "Config Path: " path
+
+if [ ! -d path ] 
+then
+    echo "Directory $path DOES NOT exists." 
+    exit 1 # die
+fi
+
 read -e -i "zabbix_agent2.conf" -p "Zabbix agent config file: " config_file_name
 read -e -i "zabbix_agentd.psk" -p "Zabbix agent PSK file: " psk_file_name
 read -e -i $HOSTNAME -p "PSK identity  (you have to set the same in Zabbix host configuration) : " pskidentity
@@ -16,6 +23,9 @@ echo "TLSPSKIdentity=$pskidentity" >> $config_file_name
 
 echo '----------------------------------------'
 echo 'Please config your Zabbix host with this :'
+echo ' '
 echo "PSK Identity : $pskidentity"
 echo "PSK Key : $(cat $psk_file_name)"
+echo ' '
+echo 'Dont forget to reload / restart your zabbix agent'
 echo '----------------------------------------'
